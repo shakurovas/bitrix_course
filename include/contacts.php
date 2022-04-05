5e1fc1c92b0da60d11098ca88aca49576b9dc606
Svetlana <shakurovas@bk.ru>
2022-04-05 12:54:38 +0000

    внедрена форма обратной связи, убрано поле "Тема письма",  создана включаемая область для контактов компании справа на странице

---
+++ include/contacts.php
@@ -0,0 +1,24 @@
+<div class="col-lg-4">
+	<div class="p-4 mb-3 bg-white">
+		<h3 class="h6 text-black mb-3 text-uppercase">Контактная информация</h3>
+		<p class="mb-0 font-weight-bold">
+			 Адрес
+		</p>
+		<p class="mb-4">
+			 Россия, г. Москва, Кутузовский проспект, 1кА (м. Парк Победы, выход 4)
+		</p>
+		<p class="mb-0 font-weight-bold">
+			 Телефон
+		</p>
+		<p class="mb-4">
+ <a href="tel: +12323235324">+1 232 3235 324</a>
+		</p>
+		<p class="mb-0 font-weight-bold">
+			 Email
+		</p>
+		<p class="mb-0">
+ <a href="mailto:info@domain.com">info@domain.com</a>
+		</p>
+	</div>
+</div>
+ <br>
